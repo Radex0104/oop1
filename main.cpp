524a953b85e6cba97279e86e9a2ba696be16d4a1
Radex0104 <126239465+Radex0104@users.noreply.github.com>
2024-02-25 17:32:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ assert(map({1,2,3,4,5},[] (int x) {x*x}) == {1,4,9,16,25});
 assert(filter({1,2,3,4,5}, [] (int x) {if (number % 2 == 0)
         return true;
     else
-        return false;})== {2,4})
+        return false;})== {2,4});
 assert(square(2) == 4);
 assert(is_even(2) == true);
 int main() {
