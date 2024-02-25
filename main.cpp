69606fa21837a06565a6075ee98eb57c12ea1a22
Radex0104 <126239465+Radex0104@users.noreply.github.com>
2024-02-25 16:52:24 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+
+std::vector<int> map(std::vector<int> numbers, int (*func)(int)) {
+    std::vector<int> result;
+    for (int num : numbers) {
+        result.push_back(func(num));
+    }
+    return result;
+}
+
+std::vector<int> filter(std::vector<int> numbers, bool (*func)(int)) {
+    std::vector<int> result;
+    for (int num : numbers) {
+        if (func(num) == true)
+        result.push_back(num);
+    }
+    return result;
+}
+
+int square(int x) {
+    return x * x;
+}
+
+bool is_even(int number) {
+    if (number % 2 == 0)
+        return true;
+    else
+        return false;
+}
+assert(map({1,2,3,4,5},[] (int x) {x*x}) == {1,4,9,16,25});
+assert(filter({1,2,3,4,5}, [] (int x) {if (number % 2 == 0)
+        return true;
+    else
+        return false;})== {2,4})
+assert(square(2) == 4);
+assert(is_even(2) == true);
+int main() {
+    std::vector<int> numbers = { 1, 2, 3, 4, 5 };
+    auto results = map(numbers, square);
+    auto results2 = filter(numbers, is_even);
+    return 0;
+}
