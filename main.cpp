cccbdc9e678c4bc3b7d02fb41842b3fc0141a573
Radex0104 <126239465+Radex0104@users.noreply.github.com>
2024-02-26 21:18:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,55 @@
 #include<iostream>
 #include<vector>
 #include<cassert>
+using namespace std;
 
 std::vector<int> map(std::vector<int> numbers, int (*func)(int)) {
     std::vector<int> result;
+    assert(!numbers.empty());
     for (int num : numbers) {
         result.push_back(func(num));
     }
-    assert(square(2) == 4);
     return result;
 }
 
-std::vector<int> filter(std::vector<int> numbers, bool (*func)(int)) {
+std::vector<int> filter(vector<int> numbers, bool (*func)(int)) {
     std::vector<int> result;
+    assert(!numbers.empty());
     for (int num : numbers) {
         if (func(num) == true)
-        result.push_back(num);
+            result.push_back(num);
     }
-    assert(is_even(2) == true);
     return result;
 }
 
-std::int square(int x) {
+int square(int x) {
     return x * x;
 }
-std::bool is_even(int number) {
+
+bool is_even(int number) {
     if (number % 2 == 0)
         return true;
     else
         return false;
 }
+
+void test()
+{
+    assert(square(2) == 4);
+    assert(is_even(2) == true);
+}
 int main() {
+    
     std::vector<int> numbers = { 1, 2, 3, 4, 5 };
+    test();
     auto results = map(numbers, square);
+    for(auto num : results)
+        cout << num << " ";
+    cout << endl;
     auto results2 = filter(numbers, is_even);
+    for (auto nums : results2)
+        cout << nums << " ";
+    cout << endl;
+    system("pause");
+    return 0;
 }
