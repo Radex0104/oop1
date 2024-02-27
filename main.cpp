2a963a21b494e9816404a748ec248b488adf23cd
Radex0104 <126239465+Radex0104@users.noreply.github.com>
2024-02-27 15:30:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@
 int main() {
     
     vector<int> numbers = { 1, 2, 3, 4, 5 };
-    test(numbers);
-    auto results = map(numbers, square);
+    n1::test(numbers);
+    auto results = n1::map(numbers, square);
     for(auto num : results)
         cout << num << " ";
     cout << endl;
-    auto results2 = filter(numbers, is_even);
+    auto results2 = n1::filter(numbers, is_even);
     for (auto nums : results2)
         cout << nums << " ";
     cout << endl;
+    n2::testIsEven();
+    n2::game();
     return 0;
 }
