b98c6a439fd997507f07b28bc10b838b12a56d79
Radex0104 <126239465+Radex0104@users.noreply.github.com>
2024-02-27 15:19:29 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,7 +1,35 @@
 #include<iostream>
 #include<vector>
 #include<cassert>
-void test(std::vector<int> numbers){
+using namespace std;
+vector<int> map(std::vector<int> numbers, int (*func)(int)) {
+    vector<int> result;
+    for (int num : numbers) {
+        result.push_back(func(num));
+    }
+    return result;
+}
+
+vector<int> filter(vector<int> numbers, bool (*func)(int)) {
+    vector<int> result;
+    for (int num : numbers) {
+        if (func(num) == true)
+            result.push_back(num);
+    }
+    return result;
+}
+
+int square(int x) {
+    return x * x;
+}
+
+bool is_even(int number) {
+    if (number % 2 == 0)
+        return true;
+    else
+        return false;
+}
+void test(vector<int> numbers){
   assert(!numbers.empty());
   assert(square(2) == 4);
   assert(is_even(2) == true);
