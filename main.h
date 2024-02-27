4b0ee9f0e09a3553d7a2e0c1955db736cdcd290a
Radex0104 <126239465+Radex0104@users.noreply.github.com>
2024-02-27 15:10:22 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<cassert>
-void test(){
+void test(vector<int> numbers){
   assert(!numbers.empty());
   assert(square(2) == 4);
   assert(is_even(2) == true);
